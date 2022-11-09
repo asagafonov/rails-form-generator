83b38884094b16d91975498af66e46931eea30ff
asagafonov <a.s.agafonov@yandex.ru>
2022-11-09 12:03:51 +0300

    add tag generation

--- hexlet_code/lib/hexlet_code.rb
+++ hexlet_code/lib/hexlet_code.rb
@@ -6,7 +6,31 @@ module HexletCode
   class Error < StandardError; end
 
   class Tag
-    def build(tag, params)
+    @@single_tags = %w[br hr img input link]
+
+    class << self
+      def build(tag, params = {})
+        params_string = params.empty? ? "" : " #{params_to_s(params)}"
+        text_content = block_given? ? yield : ""
+
+        single_tag_output = "<#{tag}#{params_string} />"
+        double_tag_output = "<#{tag}#{params_string}>#{text_content}</#{tag}>"
+
+        single_tag?(tag) ? single_tag_output : double_tag_output
+      end
+
+      private
+
+      def single_tag?(tag)
+        @@single_tags.include?(tag)
+      end
+
+      def params_to_s(params)
+        params
+          .keys
+          .map { |key| "#{key}=\"#{params[key]}\"" }
+          .join(" ")
+      end
     end
   end
 end
