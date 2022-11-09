88a172f2e9aefb844db6792695f73930ac07bd7b
asagafonov <a.s.agafonov@yandex.ru>
2022-11-09 10:57:52 +0300

    initial commit

---
+++ hexlet_code/lib/hexlet_code.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "hexlet_code/version"
+
+module HexletCode
+  class Error < StandardError; end
+
+  class Tag
+    def build(tag, params)
+    end
+  end
+end
