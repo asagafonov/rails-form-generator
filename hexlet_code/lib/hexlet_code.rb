ba1fcaf17d809b712f021bd9518159f1ac313486
asagafonov <a.s.agafonov@yandex.ru>
2022-11-11 09:21:07 +0300

    upd form_for, add input methods

--- hexlet_code/lib/hexlet_code.rb
+++ hexlet_code/lib/hexlet_code.rb
@@ -2,10 +2,50 @@
 
 require_relative "hexlet_code/version"
 
+# rubocop:disable Style/GlobalVars
+
+$tmp = []
+
 module HexletCode
   class << self
-    def form_for(_user, url = "#")
-      "<form action=\"#{url}\" method=\"post\"></form>"
+    def form_for(user, url: "#")
+      user.extend(Input)
+
+      yield(user) if block_given?
+
+      inputs = $tmp.join
+      $tmp.clear
+      [
+        "<form action=\"#{url}\" method=\"post\">",
+        inputs,
+        "</form>"
+      ].join
+    end
+
+    module Input
+      def input(field, **kwargs)
+        raise unless public_send(field)
+
+        fixed_keys = %i[as cols rows]
+        attributes = kwargs.keys.reject { |key| fixed_keys.include?(key) }.map { |key| "#{key}=\"#{kwargs[key]}\"" }
+        attributes_as_s = attributes.empty? ? "" : " #{attributes.join(" ")}"
+
+        $tmp << if kwargs[:as] == "text"
+                  build_textarea(field, attributes_as_s, kwargs)
+                else
+                  build_input(field, attributes_as_s)
+                end
+      end
+
+      def build_input(field, attrs)
+        "<input name=\"#{field}\" type=\"text\" value=\"#{public_send(field)}\"#{attrs} />"
+      end
+
+      def build_textarea(field, attrs, options)
+        cols = options[:cols] || 20
+        rows = options[:rows] || 40
+        "<textarea name=\"#{field}\" cols=\"#{cols}\" rows=\"#{rows}\"#{attrs}>#{public_send(field)}</textarea>"
+      end
     end
   end
 
@@ -40,3 +80,5 @@ module HexletCode
     end
   end
 end
+
+# rubocop:enable Style/GlobalVars
