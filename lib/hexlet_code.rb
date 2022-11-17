6b993a478bf7c9c857dc3301b34c304b8a8d26a2
asagafonov <a.s.agafonov@yandex.ru>
2022-11-17 11:13:53 +0300

    upd folder structure & paths

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,7 +3,7 @@
 require_relative 'hexlet_code/version'
 
 module HexletCode
-  autoload(:FormView, './lib/form_view')
+  autoload(:FormView, 'hexlet_code/form_view')
 
   def self.form_for(user, **kwargs)
     f = FormView.new(user, kwargs)
